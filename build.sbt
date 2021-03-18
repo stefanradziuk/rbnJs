4fca219cfb10ba12128ec3b6305890dcc52e44bb
Stefan Radziuk <stefan.radziuk19@imperial.ac.uk>
2021-03-18 20:49:27 +0000

    add popup toggling

--- build.sbt
+++ build.sbt
@@ -40,7 +40,6 @@ scalacOptions ++= Seq(
   "-Ywarn-unused:params", // Warn if a value parameter is unused.
   "-Ywarn-unused:patvars", // Warn if a variable bound in a pattern is unused.
   "-Ywarn-unused:privates", // Warn if a private member is unused.
-  "-Ywarn-value-discard", // Warn when non-Unit expression results are unused.
   "-Ybackend-parallelism",
   "8", // Enable paralellisation — change to desired number!
   "-Ycache-plugin-class-loader:last-modified", // Enables caching of classloaders for compiler plugins
--- src/main/scala/uk/radzi/rbn/Main.scala
+++ src/main/scala/uk/radzi/rbn/Main.scala
@@ -2,11 +2,15 @@ package uk.radzi.rbn
 
 import uk.radzi.rbn.controller.MainController
 import scala.util.Random
+import scala.scalajs.js.annotation.JSExportTopLevel
+import org.scalajs.dom.document
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
-    val _ = new MainController(Random.nextLong())
-  }
+  def main(args: Array[String]): Unit = new MainController(Random.nextLong())
+
+  @JSExportTopLevel("openInfo")
+  def openInfo(): Unit =
+    document.getElementById("infoPopup").classList.toggle("visiblePopup")
 
 }
